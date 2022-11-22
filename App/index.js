3f59995feee4f03e37a30f3127035c089f1ab383
zero <56087611+artemlusakov@users.noreply.github.com>
2022-11-22 11:24:43 +0300

    Add confid

---
+++ App/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const path = require('path');
+const config = require('./config/db');
+
+const app = express();
+
+const port = 3000;
+
+mongoose.connect(config.db, { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connection.on('connected', () => {
+    console.log("Successful connection to the database")
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log("Not successful connection to the database" + err)
+});
+
+app.listen(port, () => {
+    console.log("The server was running on the port:" + port)
+});
+
+app.get('/', (req, res) => {
+    res.send("Home page")
+});
